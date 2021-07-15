d3eeb8cd0121e625c207b8e2bf1d0ee6f772aaa1
Dmitriy Nozhkin <dmitrilay@gmail.com>
2021-07-15 08:07:26 +0600

    Перевод представлений в класы

--- shop/urls.py
+++ shop/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from . import views
 from .views import CategoryDetailView
+from .views import ProductDetailView
 
 app_name = 'shop'
 
 urlpatterns = [
-    # path('category/telefony/', views.category_p1, name='category_detail'),
+    path('<slug:product_slug>/', ProductDetailView.as_view(), name='product_detail'),
+
+    path('buying/', views.BuyingUp.as_view(), name='buying_up'),
+    path('', views.HomeListView.as_view(), name='StartPageViews'),
+
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
-    path('category/', views.category_list, name='category_list'),
-    path('skupka/', views.skupka, name='SkupkaPageViews'),
+    path('category/', views.CategoryListView.as_view(), name='category_list'),
+
     path('product/', views.product_list, name='product_list'),
     path('category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
-    path('', views.StartPageViews, name='StartPageViews'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product, ProductImage
 from cart.forms import CartAddProductForm
 from django.core.paginator import Paginator
+
 from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.views.generic import TemplateView
+
 from specs.models import ProductFeatures
 from django.db.models import Q
-
-
-def category_p1(request):
-    category = '131414 ' * 565
-    context = {'category': category}
-
-    return render(request, 'shop/category_detail.html', context)
 
 
 class CategoryDetailView(DetailView):
@@ -134,22 +131,53 @@ def product_detail(request, category_slug, product_slug):
     return render(request, 'product_2/product.html', context)
 
 
-def StartPageViews(request):
-    products = Product.objects.filter(available=True)[0:8]
-    category = 1
-    context = {'category': category, 'products': products, }
-
-    return render(request, 'shop/home_page/home_page.html', context)
+class HomeListView(ListView):
+    model = Product
+    template_name = 'shop/home_page/home_page.html'
+    context_object_name = 'products'
 
 
-def skupka(request):
-    category = 1
-    context = {'category': category}
-
-    return render(request, 'skupka.html', context)
+class BuyingUp(TemplateView):
+    template_name = 'shop/buying_up/buying_up.html'
 
 
-def category_list(request):
-    categories = Category.objects.all()
-    context = {'categories': categories}
-    return render(request, 'category.html', context)
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category.html'
+    context_object_name = 'categories'
+
+    # queryset = Category.objects.all()
+    # context_object_name = 'category'
+    # slug_url_kwarg = 'slug'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['categories'] = queryset = Category.objects.all()
+    #     return context
+    #
+    # def get_queryset(self):
+    #     pass
+
+
+class ProductDetailView(DetailView):
+    model = Product  # Модель шаблона
+    template_name = 'test.html'  # Путь и имя шаблона
+    context_object_name = 'product'  # Какое имя необходимо отображать в шаблоне?
+    # slug_field = 'slug' # Как называется slug в шаблоне?
+    slug_url_kwarg = 'product_slug'  # Какое имя(slug) искать в url?
+
+    # queryset = Product.objects.all()
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     print('\n\n\n')
+    #     print(context)
+    #     return context
+
+    # def get_queryset(self, **kwargs):
+    #     # print(**kwargs)
+    #     # context = super().get_context_data(**kwargs)
+    #     # queryset = get_object_or_404(Product, slug=self.slug_url_kwarg, available=True)
+    #     # queryset = Product.objects.filter(slug=self.slug_url_kwarg)
+    #     # print(context)
+    #     return 1
