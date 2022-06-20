9d41c05b0c695be04c8ef33df6ca989b3309e576
dmitrilay <dmitrilay@gmail.com>
2022-06-21 00:37:00 +0600

    Стилизация главной страницы

--- account/urls.py
+++ account/urls.py
@@ -10,6 +10,8 @@ p8 = 'password_reset/done/'
 p9 = 'reset/<uidb64>/<token>/'
 p10 = 'reset/done/'
 
+app_name = 'account'
+
 urlpatterns = [
     # Авторизация
     path('login/', views.user_login.as_view(), name='login'),
@@ -21,12 +23,16 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     # Смена пароля
     path(p5, auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path(p6, auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path(p6, auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
     # Востановления пароля
     path(p7, auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path(p8, auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path(p9, auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path(p10, auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path(p8, auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path(p9, auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path(p10, auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
     # Регистрация пользователя
     path('register/', views.register, name='register'),
     # Изменения профиля
