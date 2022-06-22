b094f90c82b0bfef7de0a76be8c6fa2c8e3ad0d4
dmitrilay <dmitrilay@gmail.com>
2022-06-22 14:15:21 +0600

    Верстка 'категории товаров'

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length=200, db_index=True, verbose_name='бренд')
+    name = models.CharField(
+        max_length=200, db_index=True, verbose_name='бренд')
 
     class Meta:
         ordering = ('name',)
@@ -17,6 +18,17 @@ class Brand(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='img_category',
+                              verbose_name="Изображение", blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            print(self.image)
+            self.image = 'img_category/test.webp'
+        elif not self.image:
+            print("Пестота")
+            self.image = 'img_default/no_image.jpg'
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ('name',)
@@ -33,8 +45,10 @@ class Category(models.Model):
 class Product(models.Model):
     brand = models.ForeignKey('Brand', related_name='brands', on_delete=models.SET_NULL, blank=False, null=True,
                               verbose_name='бренд')
-    condition = models.CharField(max_length=100, db_index=True, blank=False, verbose_name='состояние')
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    condition = models.CharField(
+        max_length=100, db_index=True, blank=False, verbose_name='состояние')
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     name_spec = models.CharField(max_length=200, db_index=True)
@@ -60,12 +74,16 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, blank=True,
                                 null=True, default=None, on_delete=models.CASCADE, verbose_name="Продукт")
-    image = models.ImageField(upload_to='products_images/', verbose_name="Изображение")
-    is_main = models.BooleanField(default=False, verbose_name="Главное изображение")
-    is_active = models.BooleanField(default=True, verbose_name="Показывать изображение")
+    image = models.ImageField(
+        upload_to='products_images/', verbose_name="Изображение")
+    is_main = models.BooleanField(
+        default=False, verbose_name="Главное изображение")
+    is_active = models.BooleanField(
+        default=True, verbose_name="Показывать изображение")
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-    name = models.CharField(max_length=200, db_index=True, default=1, verbose_name="Имя")
+    name = models.CharField(max_length=200, db_index=True,
+                            default=1, verbose_name="Имя")
 
     def __str__(self):
         return "%s" % self.pk
