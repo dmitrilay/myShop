ef03391b353657110678c1d452e10d5754a5fa87
dmitrilay <dmitrilay@gmail.com>
2022-09-02 00:56:04 +0600

    fix - запрет изменений input и верстка цены

--- cart/forms.py
+++ cart/forms.py
@@ -5,7 +5,7 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 4)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.IntegerField(label='Количество', widget=forms.TextInput(
-        attrs={'class': 'quantity-pr__input', 'value': '1'}))
+        attrs={'class': 'quantity-pr__input', 'value': '1', 'readonly': ''}))
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
 
 
