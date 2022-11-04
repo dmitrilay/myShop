ac681006d7ef2cc3787e57ef56bfc67daba6497e
Dmitriy Nozhkin <dmitrilay@gmail.com>
2022-11-04 22:39:19 +0600

    id65 Перенос кода в отдельный файл

---
+++ smartlombardAPI/modules/smartlombardAJAX.py
@@ -0,0 +1,140 @@
+import datetime
+import os
+from myshop.settings import BASE_DIR
+import urllib.parse
+from django.utils.encoding import smart_str
+import json
+from smartlombardAPI.models import ProductCRM
+from shop.models import Product
+
+
+def write_file(obj, name):
+    today = datetime.datetime.today()
+    data = today.strftime("%Y-%m-%d-%H.%M.%S")
+    _path = os.path.join(BASE_DIR, 'smartlombard', f'{name}_{data}.txt')
+    with open(_path, 'w', encoding='utf-8') as f:
+        f.write(obj)
+
+
+def data_conversion(request):
+    encodedStr = request.body.decode('utf-8')
+    _print = urllib.parse.unquote_plus(encodedStr)
+    _print = smart_str(_print, encoding='unicode',)
+    start_crop = _print.find('[', 0)
+    end_crop = _print.rfind(']', 0)+1
+    _print = _print[start_crop:end_crop]
+    _print = json.loads(_print)
+    return _print
+
+
+def type_operation(obj):
+    new_product, merchants = '', ''
+    data, *_ = obj
+
+    if data.get('data'):
+        if data['data'].get('goods'):
+            new_product = data['data']['goods']
+        if data['data'].get('merchants'):
+            merchants = data['data']['merchants']
+
+    return (new_product, merchants)
+
+
+class AddingEditingProduct():
+    def __init__(self, status, add_type, edit_type, remove_type) -> None:
+        self.add_type = add_type
+        self.edit_type = edit_type
+        self.remove_type = remove_type
+        self.status = status
+        self.main_list_product = None
+        self.crm_list_product = None
+        self.index_letter = 'u'
+        self.get_product()
+
+    def add_status(self, type):
+        self.status.append({"status": True, "type": type, "unique": '1', })
+
+    def add_product(self):
+        """Добавление нового товара"""
+        new_product, bulk_list = self.add_type, []
+
+        for e in new_product:
+            write_permission_main, write_permission_crm = self.product_search_in_list(e['article'])
+
+            if write_permission_main == True and write_permission_crm == True:
+                if e.get("sold", 'f') == None:
+                    print(e["sold"])
+                bulk_list.append(ProductCRM(name=e['name'],
+                                            article=f"{self.index_letter}{e['article']}",
+                                            price=e['price'],
+                                            features=e['features'],
+                                            category=e['category'],
+                                            subcategory=e['subcategory'],
+                                            # sold=e['sold'] if e['sold'] else 0,
+                                            condition='used',
+                                            storage=1
+                                            ))
+
+            self.add_status("good-add")
+        ProductCRM.objects.bulk_create(bulk_list)
+
+    def edit_product(self):
+        """Редактирование остатков товара"""
+        new_product = self.edit_type
+        for e in new_product:
+            main_list_product, crm_list_product = self.product_search_in_list(e['article'])
+
+            args = {'sold': True, 'storage': 0} if e['sold'] else {'sold': False, 'storage': 1}
+            article = f"{self.index_letter}{e['article']}"
+            if not crm_list_product == True:
+                ProductCRM.objects.filter(article=article).update(**args)
+
+            if not main_list_product == True:
+                Product.objects.filter(id_crm=article).update(available=False, **args)
+
+            self.add_status("good-edit")
+
+    def remove_product(self):
+        self.add_status("good-remove")
+
+    def get_product(self):
+        q = Product.objects.exclude(id_crm='').values_list('id_crm')
+        self.main_list_product = [x[0] for x in q]
+
+        q = ProductCRM.objects.all().values_list('article')
+        self.crm_list_product = [x[0] for x in q]
+
+    def product_search_in_list(self, elem):
+        elem = f'{self.index_letter}{elem}'
+        u1 = True if not elem in self.main_list_product else False
+        u2 = True if not elem in self.crm_list_product else False
+        return [u1, u2]
+
+
+def decomposition_data(products):
+    edit_type, add_type, remove_type = [], [], []
+    for product in products:
+        if product['type'] == 'add':
+            add_type.append(product['data'])
+        elif product['type'] == 'edit':
+            edit_type.append(product['data'])
+        elif product['type'] == 'remove':
+            remove_type.append(product)
+
+    return (add_type, edit_type,  remove_type)
+
+
+def create_new_shop(status, merchants):
+    """Добавление нового магазина"""
+    for event in merchants:
+        _event = event['data'] if event.get('data') else 0
+
+        if event['type'] == 'add':
+            write_file(f'{_event}', name='merchant_add')
+            status.append({"status": True, "type": "merchant-add", "unique": '1', })
+        elif event['type'] == 'edit':
+            write_file(f'{_event}', name='merchant_edit')
+            status.append({"status": True, "type": "merchant-edit", "unique": '1', })
+        elif event['type'] == 'remove':
+            write_file(f'{event}', name='merchant_remove')
+            status.append({"status": True, "type": "merchant-remove", "unique": '1', })
--- smartlombardAPI/views.py
+++ smartlombardAPI/views.py
@@ -1,114 +1,27 @@
-import datetime
 from itertools import product
 import json
-import os
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.encoding import smart_str
-from myshop.settings import BASE_DIR
 import urllib.parse
-
 from shop.models import Product
 from .models import *
-
 from django.db import connection
+from .modules.smartlombardAJAX import *
 
 
 @csrf_exempt
 def smartlombardAJAX(request):
-
-    def write_file(obj, name):
-        today = datetime.datetime.today()
-        data = today.strftime("%Y-%m-%d-%H.%M.%S")
-        _path = os.path.join(BASE_DIR, 'smartlombard', f'{name}_{data}.txt')
-        with open(_path, 'w', encoding='utf-8') as f:
-            f.write(obj)
-
     write_file(f'{request.body}', 'body')
-
-    encodedStr = request.body.decode('utf-8')
-    _print = urllib.parse.unquote_plus(encodedStr)
-    _print = smart_str(_print, encoding='unicode',)
-    start_crop = _print.find('[', 0)
-    end_crop = _print.rfind(']', 0)+1
-    _print = _print[start_crop:end_crop]
-    _print = json.loads(_print)
-
-    new_product, merchants = '', ''
-    if _print[0].get('data'):
-        if _print[0]['data'].get('goods'):
-            new_product = _print[0]['data']['goods']
-        if _print[0]['data'].get('merchants'):
-            merchants = _print[0]['data']['merchants']
-
+    data = data_conversion(request)
+    new_product, merchants = type_operation(data)
     status = []
+    add_type, edit_type, remove_type = decomposition_data(new_product)
+    p = AddingEditingProduct(status, add_type, edit_type, remove_type)
+    p.add_product()
+    p.edit_product()
+    create_new_shop(status, merchants)
 
-    # Добавление нового товара
-    article_in_database_main = Product.objects.filter(id_crm__gte=0).values_list('id_crm')
-    article_in_database_main = list(map(lambda _i: _i[0], article_in_database_main))
-
-    article_in_database = ProductCRM.objects.all().values_list('article')
-    article_in_database = list(map(lambda _i: _i[0], article_in_database))
-
-    bulk_list = []
-    for event in new_product:
-        _event = event['data'] if event.get('data') else 0
-
-        search_product_1 = True if int(_event['article']) in article_in_database_main else False
-        search_product_2 = True if int(_event['article']) in article_in_database else False
-
-        if event['type'] == 'add':
-            if search_product_1 == False and search_product_2 == False:
-                bulk_list.append(ProductCRM(name=_event['name'],
-                                            article=_event['article'],
-                                            price=_event['price'],
-                                            features=_event['features'],
-                                            category=_event['category'],
-                                            subcategory=_event['subcategory'],
-                                            hidden=_event['hidden'] if _event.get('hidden') else 0,
-                                            sold=_event['sold'] if _event.get('sold') else 0,
-                                            condition='used'
-                                            ))
-
-            status.append({"status": True, "type": "good-add", "unique": '1', })
-        elif event['type'] == 'edit':
-
-            print(event['data'])
-
-            if search_product_2 == True:
-                if _event['sold'] == True:
-                    ProductCRM.objects.filter(article=_event['article']).update(sold=True, hidden=True)
-                elif _event['sold'] == False:
-                    ProductCRM.objects.filter(article=_event['article']).update(sold=False, hidden=False)
-
-            if search_product_1 == True:
-                if _event['sold'] == True:
-                    Product.objects.filter(id_crm=_event['article']).update(available=False, sold=True, storage=0)
-                elif _event['sold'] == False:
-                    Product.objects.filter(id_crm=_event['article']).update(available=False, sold=False, storage=1)
-
-            status.append({"status": True, "type": "good-edit", "unique": '1', })
-        elif event['type'] == 'remove':
-            status.append({"status": True, "type": "good-remove", "unique": '1', })
-
-    ProductCRM.objects.bulk_create(bulk_list)
-
-    # Добавление нового магазина
-    for event in merchants:
-        _event = event['data'] if event.get('data') else 0
-
-        if event['type'] == 'add':
-            write_file(f'{_event}', name='merchant_add')
-            status.append({"status": True, "type": "merchant-add", "unique": '1', })
-        elif event['type'] == 'edit':
-            write_file(f'{_event}', name='merchant_edit')
-            status.append({"status": True, "type": "merchant-edit", "unique": '1', })
-        elif event['type'] == 'remove':
-            write_file(f'{event}', name='merchant_remove')
-            status.append({"status": True, "type": "merchant-remove", "unique": '1', })
-
-    # print(status)
     return JsonResponse(status, safe=False)
 
 
