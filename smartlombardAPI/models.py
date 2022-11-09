4cc2d46097dd39348e1c1380ef5a6fa1f55082c2
Dmitriy Nozhkin <dmitrilay@gmail.com>
2022-11-10 00:21:43 +0600

    Рефакторинг models Mixin

--- smartlombardAPI/models.py
+++ smartlombardAPI/models.py
@@ -1,17 +1,14 @@
-from tabnanny import verbose
 from django.db import models
 import itertools
-# from django.utils.text import slugify
 from pytils.translit import slugify
 from django.core.validators import FileExtensionValidator
 
 
-class ProductCRM(models.Model):
+class BaseProductCrmMixin(models.Model):
     MONTH_CHOICES = (
         ("new", "Новый"),
         ("used", "б/у"),
     )
-
     name = models.CharField(max_length=200, db_index=True, verbose_name='название')
     slug = models.SlugField(default='', blank=True)
     name_spec = models.CharField(max_length=200, db_index=True, blank=True,
@@ -26,114 +23,54 @@ class ProductCRM(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='цена')
     features = models.TextField(blank=True, verbose_name='описание товара')
     category = models.CharField(max_length=200, db_index=True, verbose_name='категория')
-    subcategory = models.CharField(max_length=200, db_index=True, verbose_name='подкатегория', blank=True)
-    sold = models.BooleanField(default=False, null=True, verbose_name='товар является проданным')
+    subcategory = models.CharField(max_length=200, verbose_name='подкатегория', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     available = models.BooleanField(default=False, verbose_name='Подтвердить модерацию')
-    storage = models.IntegerField(verbose_name='Остатки')
+    storage = models.IntegerField(verbose_name='Остатки', default=1)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+    def _generate_slug(self):
+        value = self.name
+        slug_candidate = slug_original = slugify(value)
+        for i in itertools.count(1):
+            if not ProductCRM.objects.filter(slug=slug_candidate).exists():
+                break
+            slug_candidate = '{}-{}'.format(slug_original, i)
+
+        self.slug = slug_candidate
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self._generate_slug()
+        elif self.slug == '':
+            self._generate_slug()
+
+        super().save(*args, **kwargs)
+
+
+class ProductCRM(BaseProductCrmMixin):
+    sold = models.BooleanField(default=False, null=True, verbose_name='товар является проданным')
 
     class Meta:
         verbose_name = "Товары из CRM"
         verbose_name_plural = "2. Б/У товары из CRM"
 
-    def __str__(self):
-        return self.name
 
-    def _generate_slug(self):
-        # max_length = self._meta.get_field('slug').max_length
-        value = self.name
-        # slug_candidate = slug_original = slugify(value, allow_unicode=False)
-        slug_candidate = slug_original = slugify(value)
-        for i in itertools.count(1):
-            if not ProductCRM.objects.filter(slug=slug_candidate).exists():
-                break
-            slug_candidate = '{}-{}'.format(slug_original, i)
-
-        self.slug = slug_candidate
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self._generate_slug()
-        elif self.slug == '':
-            self._generate_slug()
-
-        super().save(*args, **kwargs)
-
-
-class OldProductCrmImage(models.Model):
-    product = models.ForeignKey(ProductCRM, related_name='productSET', blank=True, null=True, default=None,
-                                on_delete=models.CASCADE, verbose_name="Продукт")
-    image = models.ImageField(upload_to='garbage/', verbose_name="Изображение")
-
-    def __str__(self):
-        return "%s" % self.image
-
-    class Meta:
-        verbose_name = 'Фотографию'
-        verbose_name_plural = 'Фотографии OldProductCrmImage'
-
-
-class NewProductCRM(models.Model):
-    MONTH_CHOICES = (
-        ("new", "Новый"),
-        ("used", "б/у"),
-    )
-
-    name = models.CharField(max_length=200, db_index=True, verbose_name='название')
-    slug = models.SlugField(default='', blank=True)
-    name_spec = models.CharField(max_length=200, db_index=True, blank=True,
-                                 verbose_name='название для поиска характеристик')
-    url_spec = models.URLField(blank=True, null=True, verbose_name='URL на карточку товара')
-    uploading_csv_file = models.FileField(upload_to='uploads/', blank=True, null=True,
-                                          verbose_name='Вложение с характеристиками (CSV)',
-                                          validators=[FileExtensionValidator(['csv'])])
-    condition = models.CharField(choices=MONTH_CHOICES, default=MONTH_CHOICES[1], max_length=200,
-                                 db_index=True, verbose_name='состояние')
-    article = models.CharField(max_length=20, verbose_name='id продукта в CRM')
-    price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='цена')
-    features = models.TextField(blank=True, verbose_name='описание товара')
-    category = models.CharField(max_length=200, db_index=True, verbose_name='категория')
-    subcategory = models.CharField(max_length=200, db_index=True,
-                                   verbose_name='подкатегория', blank=True, null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    available = models.BooleanField(default=False, verbose_name='Подтвердить модерацию')
-    storage = models.IntegerField(verbose_name='Остатки')
-
+class NewProductCRM(BaseProductCrmMixin):
     class Meta:
         verbose_name = "Товары(NEW) из CRM"
         verbose_name_plural = "1. Новые товары из CRM"
 
-    def __str__(self):
-        return self.name
 
-    def _generate_slug(self):
-        # max_length = self._meta.get_field('slug').max_length
-        value = self.name
-        # slug_candidate = slug_original = slugify(value, allow_unicode=False)
-        slug_candidate = slug_original = slugify(value)
-        for i in itertools.count(1):
-            if not ProductCRM.objects.filter(slug=slug_candidate).exists():
-                break
-            slug_candidate = '{}-{}'.format(slug_original, i)
-
-        self.slug = slug_candidate
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self._generate_slug()
-        elif self.slug == '':
-            self._generate_slug()
-
-        super().save(*args, **kwargs)
-
-
-class NewProductCrmImage(models.Model):
-    product = models.ForeignKey(NewProductCRM, related_name='productSET', blank=True, null=True, default=None,
-                                on_delete=models.CASCADE, verbose_name="Продукт")
+class BaseProductCrmImageMixin(models.Model):
     image = models.ImageField(upload_to='garbage/', verbose_name="Изображение")
+    is_main = models.BooleanField(default=False, verbose_name="Главное изображение")
 
     def __str__(self):
         return "%s" % self.image
@@ -141,3 +78,14 @@ class NewProductCrmImage(models.Model):
     class Meta:
         verbose_name = 'Фотографию'
         verbose_name_plural = 'Фотографии'
+        abstract = True
+
+
+class NewProductCrmImage(BaseProductCrmImageMixin):
+    args = {"related_name": 'productSET', "verbose_name": "Продукт"}
+    product = models.ForeignKey(NewProductCRM, blank=True, null=True, on_delete=models.CASCADE, **args)
+
+
+class OldProductCrmImage(BaseProductCrmImageMixin):
+    args = {"related_name": 'productSET', "verbose_name": "Продукт"}
+    product = models.ForeignKey(ProductCRM,  blank=True, null=True, on_delete=models.CASCADE, **args)
