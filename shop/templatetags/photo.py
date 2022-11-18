4fe85c70c04ad5de133858a94481a9f3d08d63cc
Dmitriy Nozhkin <dmitrilay@gmail.com>
2022-11-18 12:59:04 +0600

    id75 Ошибка при отсутствии фотографии

--- shop/templatetags/photo.py
+++ shop/templatetags/photo.py
@@ -6,6 +6,8 @@ register = template.Library()
 @register.inclusion_tag('common/photo.html', name='picture_tag')
 def test_tag_1(image_webp=None, image_jpeg=None):
     plug_webp, plug_jpg = '/static/img/img_default/no_image.webp', '/static/img/img_default/no_image.jpg'
-    image_webp = image_webp if image_webp else plug_webp
-    plug_jpg = plug_jpg if plug_jpg else plug_jpg
+
+    image_webp = image_webp.url if image_webp else plug_webp
+    image_jpeg = image_jpeg.url if image_jpeg else plug_jpg
+
     return {"image_webp": image_webp, "image_jpeg": image_jpeg}
